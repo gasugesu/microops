90caac923f96828ef6c594db812fff30446fa21d
Shinya Sugamoto <sugamoto@me.com>
2021-03-04 07:25:43 +0000

    completion of step6

--- net.c
+++ net.c
@@ -98,16 +98,31 @@ net_device_close(struct net_device *dev)
 }
 
 /* NOTE: must not be call after net_run() */
-int
-net_device_add_iface(struct net_device *dev, struct net_iface *iface)
+int net_device_add_iface(struct net_device *dev, struct net_iface *iface)
 {
-
+    struct net_iface *entry;
+    for (entry = dev->ifaces; entry; entry = entry->next)
+    {
+        if (entry->family == iface->next)
+        {
+            errorf("already exists, dev=%s, family=%d", dev->name, entry->family);
+            return -1;
+        }
+    }
 }
 
 struct net_iface *
 net_device_get_iface(struct net_device *dev, int family)
 {
-
+    struct net_iface *entry;
+    for (entry = dev->ifaces; entry; entry = entry->next)
+    {
+        if (entry->family == family)
+        {
+            break;
+        }
+    }
+    return entry;
 }
 
 int net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst)
